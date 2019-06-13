d463bc1855a88062e6d75ecbcdc32368d4f87866
onlydel <onlydel@realgrid.com>
2019-06-13 16:50:01 +0900

    deploy:

--- build.sh
+++ build.sh
@@ -6,37 +6,18 @@ set -e
 # build
 vuepress build . --dest ../docs
 
-# navigate into the build output directory
-cd docs
-
 # if you are deploying to a custom domain
 echo 'docs.realdesk.kr' > ../docs/CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git add .
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-# git remote add origin https://github.com/onlydel/docs.wrw.kr.git
-# git push -f origin master:gh-pages
-
-# cd -
-
-git add -A
-
-set FIRST_ARGS = $1
-if [ FIRST_ARGS = "" ] ; then
-  now=$(date +'%Y-%m-%d %T')
-  git commit -m "deploy $now"
-  # echo "none"
+if [ "$1" = "" ] ; then
+  NOW=$(date +'%Y-%m-%d %T')
+  git commit -m "deploy $NOW"
+  # echo "deploy $NOW"
 else
   git commit -m "deploy: $FIRST_ARGS"
-  # echo "$FIRST_ARGS"
+  # echo "deploy: $1"
 fi
 
 git push -f origin master
